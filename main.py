312a52998b570cd793c773bb389e738905770537
menahem_erlich <erlichmenahem@gmail.com>
2025-11-13 10:34:02 +0200

    Fence cipher

--- main.py
+++ main.py
@@ -23,6 +23,35 @@ def caesar_decryption(text: str, offset: int):
                     decrypted_text += l_list[(i - offset) % 26]
     return decrypted_text
 
+def fence_encryption(text: str):
+    encrypted_text = ""
+    l_list = []
+    for ch in text:
+        if ch != " ":
+            l_list.append(ch)
+    for i in range(len(l_list)):
+        if i % 2 == 0:
+            encrypted_text += l_list[i]
+    for i in range(len(l_list)):
+        if i % 2 != 0:
+            encrypted_text += l_list[i]
+    return encrypted_text
+
+def fence_decryption(text: str):
+    decrypted_text = ""
+    l_list = []
+    for ch in text:
+        l_list.append(ch)
+    if len(l_list) % 2 == 0:
+        middle = len(l_list) // 2
+    else:
+        middle = (len(l_list) // 2) + 1
+    l_list_1 = l_list[:middle]
+    l_list_2 = l_list[middle:]
+    for i in range(len(l_list) // 2):
+        decrypted_text += l_list_1[i]
+        decrypted_text += l_list_2[i]
+    return decrypted_text
 
 
 
